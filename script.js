efb545a228cbe4a67848d51b7b04bdbb4123fd76
Rakesh-Choudhary175 <rakeshchoudhary.rc917@gmail.com>
2023-08-04 20:00:26 +0530

    Finall Commit

--- script.js
+++ script.js
@@ -5,53 +5,40 @@ let score = 20;
 console.log(correctNumber);
 let highScore = 0;
 
+const message = message => {
+  if (score > 1) {
+    document.querySelector('.message').textContent = message;
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = '💥 You loss the game';
+    document.querySelector('.score').textContent = 0;
+  }
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (!guess) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '⛔ No Number';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You loss the game';
-      document.querySelector('.score').textContent = 0;
-    }
+    message('⛔ No Number');
   } else if (guess === correctNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
     if (score > highScore) {
       highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
-    document.querySelector('.highscore').textContent = highScore;
     document.querySelector('.number').textContent = correctNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
   } else if (guess < correctNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 to low';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You loss the game';
-      document.querySelector('.score').textContent = 0;
-    }
+    message('📉 to low');
   } else if (guess > correctNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 to high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You loss the game';
-      document.querySelector('.score').textContent = 0;
-    }
+    message('📈 to high');
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  // if (score > highScore) {
-  //   highScore = score;
-  // }
   score = 20;
   document.querySelector('.score').textContent = score;
   correctNumber = Math.trunc(Math.random() * 20) + 1;
